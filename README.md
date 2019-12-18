ad6a677eff45dd0e3c0c01a2b016d660e82beb8b
Ondrej Lehota <ondrej.lehota@uk.ibm.com>
2019-12-18 12:20:33 +0000

    Set zero duration to P0D, add comments

--- README.md
+++ README.md
@@ -3,4 +3,23 @@ iso8601duration
 
 ISO8601 Duration Parser for Golang
 
-Adapted from http://github.com/BrianHicks/finch
+Provides a partial implementation of ISO8601 durations. Parsing doesn't take into consideration variable lengths
+of days (24 vs 25), months (28 vs 31) nor years (365 vs 366). It will default to the following:
+
+* 1 Day = 24 Hours
+* 1 Month = 30 Days
+* 1 Year = 365 Days
+ 
+Also, converting back to string will omit months and work with days and weeks, e.g.
+
+```
+INPUT     OUTPUT
+P1M       P30D
+P1M5D     P5W
+P1Y1M5D   P1Y35D
+```
+
+Zero value is set to `P0D`
+
+Forked from https://github.com/ChannelMeter/iso8601duration (refers to http://github.com/BrianHicks/finch)
+Updated from https://github.com/retzkek/iso8601duration
--- duration.go
+++ duration.go
@@ -1,39 +1,94 @@
-// Package duration provides a partial implementation of ISO8601 durations. (no months)
+// Package duration provides a partial implementation of ISO8601 durations.
+// Constant values are assumed for non-constant timespans for convenience
+// 1 Day = 24 Hours
+// 1 Month = 30 Days
+// 1 Year = 365 Days
 package duration
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"math"
 	"regexp"
 	"strconv"
 	"text/template"
 	"time"
 )
 
+const (
+	// Zero is zero value for duration. ISO-8601 mandates at least one element for a zero duration (4.4.3.2.c - page 21)
+	Zero = "P0D"
+	// Day is an average day duration
+	Day = time.Hour * 24
+	// Week is an average week duration
+	Week = Day * 7
+	// Month is an average month duration
+	Month = Day * 30
+	// Year is an average year duration
+	Year = Day * 365
+)
+
 var (
 	// ErrBadFormat is returned when parsing fails
-	ErrBadFormat = errors.New("bad format string")
+	ErrBadFormat = errors.New("invalid ISO8601 string")
 
-	// ErrNoMonth is raised when a month is in the format string
-	ErrNoMonth = errors.New("no months allowed")
+	tmpl = template.Must(template.New("duration").Parse(`P{{if .IsZero}}0D{{else}}{{if and .Weeks .IsWeeksOnly}}{{.Weeks}}W{{else}}{{if .Years}}{{.Years}}Y{{end}}{{if .Days}}{{.Days}}D{{end}}{{if .HasTimePart}}T{{if .Hours}}{{.Hours}}H{{end}}{{if .Minutes}}{{.Minutes}}M{{end}}{{if .Seconds}}{{.Seconds}}S{{end}}{{end}}{{end}}{{end}}`))
 
-	tmpl = template.Must(template.New("duration").Parse(`P{{if .Years}}{{.Years}}Y{{end}}{{if .Weeks}}{{.Weeks}}W{{end}}{{if .Days}}{{.Days}}D{{end}}{{if .HasTimePart}}T{{end }}{{if .Hours}}{{.Hours}}H{{end}}{{if .Minutes}}{{.Minutes}}M{{end}}{{if .Seconds}}{{.Seconds}}S{{end}}`))
-
-	full = regexp.MustCompile(`P((?P<year>\d+)Y)?((?P<month>\d+)M)?((?P<day>\d+)D)?(T((?P<hour>\d+)H)?((?P<minute>\d+)M)?((?P<second>\d+)S)?)?`)
+	full = regexp.MustCompile(`P((?P<year>\d+)Y)?((?P<month>\d+)M)?((?P<day>\d+)D)?(T((?P<hour>\d+)H)?((?P<minute>\d+)M)?((?P<second>[\d\.]+)S)?)?`)
 	week = regexp.MustCompile(`P((?P<week>\d+)W)`)
 )
 
+// Duration is ISO8601 type for your structs that will ensure correct validation and formatting per ISO8601 standard
 type Duration struct {
-	Years   int
-	Weeks   int
-	Days    int
-	Hours   int
-	Minutes int
-	Seconds int
+	time.Duration
 }
 
-func FromString(dur string) (*Duration, error) {
+// Years returns duration in whole years
+func (d *Duration) Years() float64 {
+	x := float64(d.Duration / Year)
+	return math.Floor(x)
+}
+
+// Weeks returns duration in whole weeks
+func (d *Duration) Weeks() float64 {
+	x := d.Days()
+	y := float64(Week / Day)
+	if math.Mod(x, y) > 0 {
+		return 0
+	}
+	return x / y
+}
+
+// Days returns duration in whole days
+func (d *Duration) Days() float64 {
+	x := float64(d.Duration / Day)
+	y := float64(Year / Day)
+	return math.Mod(x, y)
+}
+
+// Hours returns duration in whole hours
+func (d *Duration) Hours() float64 {
+	x := float64(d.Duration / time.Hour)
+	y := float64(Day / time.Hour)
+	return math.Mod(x, y)
+}
+
+// Minutes returns duration in whole minutes
+func (d *Duration) Minutes() float64 {
+	x := float64(d.Duration / time.Minute)
+	y := float64(time.Hour / time.Minute)
+	return math.Mod(x, y)
+}
+
+// Seconds returns duration in whole seconds
+func (d *Duration) Seconds() float64 {
+	return (float64(d.Duration.Nanoseconds()) - math.Trunc(d.Duration.Minutes())*float64(time.Minute)) / float64(time.Second)
+}
+
+// ParseString parses string into ISO8601 duration
+func ParseString(dur string) (*Duration, error) {
 	var (
 		match []string
 		re    *regexp.Regexp
@@ -49,7 +104,7 @@ func FromString(dur string) (*Duration, error) {
 		return nil, ErrBadFormat
 	}
 
-	d := &Duration{}
+	d := time.Duration(0)
 
 	for i, name := range re.SubexpNames() {
 		part := match[i]
@@ -57,38 +112,45 @@ func FromString(dur string) (*Duration, error) {
 			continue
 		}
 
-		val, err := strconv.Atoi(part)
+		val, err := strconv.ParseFloat(part, 64)
 		if err != nil {
 			return nil, err
 		}
+
 		switch name {
 		case "year":
-			d.Years = val
+			d += time.Duration(val) * Year
 		case "month":
-			return nil, ErrNoMonth
+			d += time.Duration(val) * Month
 		case "week":
-			d.Weeks = val
+			d += time.Duration(val) * Week
 		case "day":
-			d.Days = val
+			d += time.Duration(val) * Day
 		case "hour":
-			d.Hours = val
+			d += time.Duration(val) * time.Hour
 		case "minute":
-			d.Minutes = val
+			d += time.Duration(val) * time.Minute
 		case "second":
-			d.Seconds = val
+			d += time.Duration(int(val)) * time.Second
+			// handle fractional seconds
+			val = (val - math.Trunc(val)) * 1000.0
+			d += time.Duration(int(val)) * time.Millisecond
+			val = (val - math.Trunc(val)) * 1000.0
+			d += time.Duration(int(val)) * time.Microsecond
+			val = (val - math.Trunc(val)) * 1000.0
+			d += time.Duration(int(val)) * time.Nanosecond
 		default:
-			return nil, errors.New(fmt.Sprintf("unknown field %s", name))
+			return nil, fmt.Errorf("unknown field %s", name)
 		}
 	}
 
-	return d, nil
+	return &Duration{d}, nil
 }
 
 // String prints out the value passed in. It's not strictly according to the
-// ISO spec, but it's pretty close. In particular, to completely conform it
-// would need to round up to the next largest unit. 61 seconds to 1 minute 1
-// second, for example. It would also need to disallow weeks mingling with
-// other units.
+// ISO spec, but it's pretty close. In particular, months are not returned.
+// Instead, it returns a value in days (1D ~ 364D) or weeks (1W ~ 52W)
+// whenever possible.
 func (d *Duration) String() string {
 	var s bytes.Buffer
 
@@ -100,22 +162,47 @@ func (d *Duration) String() string {
 	return s.String()
 }
 
-func (d *Duration) HasTimePart() bool {
-	return d.Hours != 0 || d.Minutes != 0 || d.Seconds != 0
+// IsZero checks whether duration is zero. Zero value is set to
+func (d *Duration) IsZero() bool {
+	return d.Duration == 0
 }
 
+// IsWeeksOnly checks whether duration in whole weeks
+func (d *Duration) IsWeeksOnly() bool {
+	return time.Duration(time.Duration(d.Weeks())*Week) == d.Duration
+}
+
+// HasTimePart checks if duration has time section
+func (d *Duration) HasTimePart() bool {
+	return d.Hours() != 0.0 || d.Minutes() != 0.0 || d.Seconds() != 0.0
+}
+
+// ToDuration converst ISO8001 duration into time.Duration
 func (d *Duration) ToDuration() time.Duration {
-	day := time.Hour * 24
-	year := day * 365
+	return d.Duration
+}
 
-	tot := time.Duration(0)
+// UnmarshalJSON validates json against Duration type
+func (d *Duration) UnmarshalJSON(data []byte) error {
+	b := bytes.NewBuffer(data)
+	dec := json.NewDecoder(b)
+	var s string
+	if err := dec.Decode(&s); err != nil {
+		return err
+	}
+	t, err := ParseString(s)
+	if err != nil {
+		return err
+	}
+	*d = *t
+	return nil
+}
 
-	tot += year * time.Duration(d.Years)
-	tot += day * 7 * time.Duration(d.Weeks)
-	tot += day * time.Duration(d.Days)
-	tot += time.Hour * time.Duration(d.Hours)
-	tot += time.Minute * time.Duration(d.Minutes)
-	tot += time.Second * time.Duration(d.Seconds)
-
-	return tot
+// MarshalJSON converts Duration into json value
+func (d Duration) MarshalJSON() ([]byte, error) {
+	var b bytes.Buffer
+	enc := json.NewEncoder(&b)
+	s := d.String()
+	enc.Encode(s)
+	return b.Bytes(), nil
 }
